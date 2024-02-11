191ba3040b88f5e9d857b7145b0c62d94e3e72fd
AnaCamarasan <ana.camarasan@gmail.com>
2024-02-11 14:57:55 +0000

    removed news.json and created db

--- graphql-server/createDatabase.js
+++ graphql-server/createDatabase.js
@@ -7,18 +7,73 @@ const db = new sqlite3.Database('./mydatabase.db', (err) => {
   } else {
     console.log('Database opened');
     db.run(
-      `CREATE TABLE users (
+      `CREATE TABLE IF NOT EXISTS users (
         id INTEGER PRIMARY KEY AUTOINCREMENT,
-        name TEXT NOT NULL,
-        email TEXT NOT NULL UNIQUE
+        firstName TEXT NOT NULL,
+        lastName TEXT NOT NULL,
+        email TEXT NOT NULL UNIQUE,
+        password TEXT NOT NULL,
+        age INTEGER NOT NULL,
+        phoneNumber TEXT NOT NULL,
+        region TEXT NOT NULL,
+        role TEXT NOT NULL,
+        gender TEXT NOT NULL
       );`,
       (err) => {
         if (err) {
-          console.error('Error creating table', err);
+          console.error('Error creating table users', err);
         } else {
-          console.log('Table created');
+          console.log('Table users created');
         }
       }
     );
+    
+    db.run(
+      `CREATE TABLE IF NOT EXISTS interests (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        interest_name TEXT NOT NULL UNIQUE
+    );`,
+      (err) => {
+        if (err) {
+          console.error('Error creating table interests', err);
+        } else {
+          console.log('Table intrests created');
+        }
+      }
+    );
+
+    db.run(
+      `CREATE TABLE IF NOT EXISTS user_interests (
+        user_id INTEGER NOT NULL,
+        interest_id INTEGER NOT NULL,
+        PRIMARY KEY (user_id, interest_id),
+        FOREIGN KEY (user_id) REFERENCES users (id),
+        FOREIGN KEY (interest_id) REFERENCES interests (id)
+    );`,
+      (err) => {
+        if (err) {
+          console.error('Error creating table user_interests', err);
+        } else {
+          console.log('Table user_intrests created');
+        }
+      }
+    );
+    
+    db.run(
+      `CREATE TABLE IF NOT EXISTS news (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        title TEXT NOT NULL,
+        excerpt TEXT NOT NULL,
+        imageUrl TEXT NOT NULL
+    );`,
+      (err) => {
+        if (err) {
+          console.error('Error creating table news', err);
+        } else {
+          console.log('Table news created');
+        }
+      }
+    );
+    
   }
 });
