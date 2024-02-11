463ca2f54db6dcd61cc87b25138cbebe7ead1a48
AnaCamarasan <ana.camarasan@gmail.com>
2024-02-11 20:16:50 +0000

    Revert "updated files"
    This reverts commit 34086fdc8b3a3626b7fdddc9b352c9c3bafd2dbc.

--- graphql-server/dist/src/resolvers/interestResolver.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.interestResolvers = void 0;
-const interestService_1 = require("../services/interestService");
-exports.interestResolvers = {
-    Query: {
-        interests: async (_, __, { db }) => (0, interestService_1.getInterests)(db),
-    },
-};
